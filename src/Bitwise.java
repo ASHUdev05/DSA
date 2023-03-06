4facd3f43f6b155203b68ceb5c9a5b3e2222e7cd
ASHUdev05 <amphoteric69@gmail.com>
2023-03-06 21:59:31 +0530

    Day 1 - 06-03-23

---
+++ src/Bitwise.java
@@ -0,0 +1,6 @@
+public class Bitwise {
+        public static void main(String[] args) {
+            System.out.println(9<<4);   // a<<b = a*2^b
+            System.out.println(9>>4);   // a>>b = a/2^b
+        }
+    }
