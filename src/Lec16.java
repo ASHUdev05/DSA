17a9f0980d42123a930eb25b64ad45132f1ba1a2
ASHUdev05 <amphoteric69@gmail.com>
2023-03-06 22:22:59 +0530

    Day 1 - 06-03-23

---
+++ src/Lec16.java
@@ -0,0 +1,27 @@
+/*
+* For an array of integers return the first value that is repeating in the array
+* */
+
+import java.util.Scanner;
+
+public class Lec16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Lec16 obj = new Lec16();
+        System.out.println("Enter size:");
+        int size = sc.nextInt();
+        int A[] = new int[size];
+        System.out.println("Enter elements:");
+        for (int i=0; i<size; i++)
+            A[i] = sc.nextInt();
+        System.out.println(obj.repeatChecker(A));
+    }
+    int repeatChecker(int X[])
+    {
+        for(int i=0; i<X.length; i++)
+            for(int j=i+1; j<X.length;j++)
+                if(X[i] == X[j])
+                    return X[i];
+        return -1;
+    }
+}
